d4243490356396c813b7c52cc632e3774e09a3ed
Jeet_Patel <kasundrajeet529@gmail.com>
2023-07-16 00:49:59 +0530

    add website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    navigation: {
+        nextEl: ".rightsideslider",
+        prevEl: ".leftsideslider",
+    },
+});
---
+++ js/root.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                Theme_color: '#AA0B28',
+            },
+            screens: {
+                'desktop': '1440px',
+                'laptop': '1140px',
+                'ipad': '990px',
+                'tablet': '768px',
+                'mobile': '440px'
+            },
+        }
+    }
+}
